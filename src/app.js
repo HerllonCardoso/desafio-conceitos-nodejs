4ad922e06d68c4f9b42b68ac891bbed73eb28f65
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-10-23 22:17:51 -0300

    feat: main routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,59 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const {title, url, techs} = request.body;
+  
+  const repository = { id: uuid(), title: title, url: url, techs: techs}
+
+  repositories.push(repository)
+
+  return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const {id} = request.params;
+
+  const repositoryIndex = repositories.find(repository => repository.id === id);
+
+  if(repositoryIndex < 0 ){
+    return response.status(400).json({message: "Repository didn't exist"})
+  }
+
+  const repository = { id: uuid(), title: title, url: url, techs: techs}
+
+  
+  repositories[repositoryIndex] = repository;
+
+
+  return response.json({message: 'Repository edited successfully'})
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  const { id } = request.params
+
+  const repositoryIndex = repositories.find(repository => repository.id === id);
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({message: 'Repository doesnt exist'})
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+    return response.json({message: 'Repository was deleted'})
+  });
+    
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
 });
 
 module.exports = app;
